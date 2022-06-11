651d160078a972fba141e6f236953924006099be
KYKY <Rizkysrg62@gmail.com>
2022-06-11 20:42:36 +0700

    Tambah Search Bar

--- clone_gojek/lib/Screens/beranda.dart
+++ clone_gojek/lib/Screens/beranda.dart
@@ -17,6 +17,12 @@ class _HomeScreenState extends State<HomeScreen> {
     "Pesanan",
     "Chat",
   ];
+  List<bool> tabBarBadgeList = [
+    false,
+    false,
+    false,
+    true,
+  ];
 
   int tabBarIndex = 0;
 
@@ -24,32 +30,63 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     Widget tabBarItem(int index) {
       return Expanded(
-        child: Container(
-          margin: EdgeInsets.all(5),
-          height: double.infinity,
-          decoration: BoxDecoration(
-              color:
-                  (tabBarIndex == index) ? MyColors.white : Colors.transparent,
-              borderRadius: BorderRadius.circular(100)),
-          child: InkWell(
-            onTap: () {
-              setState(() {
-                tabBarIndex = index;
-              });
-            },
-            child: Center(
-              child: Text(
-                tabBarList[index],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: (tabBarIndex == index)
-                        ? MyColors.darkGreen
-                        : MyColors.white,
-                    fontSize: MyFontSize.medium1,
-                    fontWeight: FontWeight.bold),
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.all(5),
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  color: (tabBarIndex == index)
+                      ? MyColors.white
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.circular(100)),
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    tabBarIndex = index;
+                  });
+                },
+                child: Center(
+                  child: Text(
+                    tabBarList[index],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: (tabBarIndex == index)
+                            ? MyColors.darkGreen
+                            : MyColors.white,
+                        fontSize: MyFontSize.medium1,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
               ),
             ),
-          ),
+            if (tabBarBadgeList[index])
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: MyColors.red,
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(
+                      width: 1.5,
+                      color: MyColors.white,
+                    ),
+                  ),
+                  child: Center(
+                    child: Container(
+                      width: 4,
+                      height: 4,
+                      decoration: BoxDecoration(
+                        color: MyColors.white,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       );
     }
@@ -73,11 +110,67 @@ class _HomeScreenState extends State<HomeScreen> {
       );
     }
 
+    Widget searchBar() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: MyColors.whiteL2,
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(
+                      width: 1.5,
+                      color: MyColors.softGrey,
+                    ),
+                  ),
+                  child: TextFormField(
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      icon: Icon(
+                        Icons.search,
+                        size: 30,
+                        color: MyColors.blackText,
+                      ),
+                      hintText: "Cari layanan, makanan, & tujuan",
+                      hintStyle: TextStyle(
+                        color: MyColors.grey,
+                        fontSize: MyFontSize.medium2,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset(
+                'assets/ic_indosend.png',
+                width: 55,
+                height: 55,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: MyColors.white,
       appBar: AppBar(
         backgroundColor: MyColors.green,
         title: tabBar(),
+      ),
+      body: ListView(
+        children: [
+          searchBar(),
+        ],
       ),
     );
   }
